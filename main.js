ef723c6acb5cf2ad868e4bc382cf254e63260d83
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-11-15 16:55:05 -0800

    overhauled the entire app

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
     var KEY_ENTER = 13;
-    var KEY_DELETE = 46;
-    var KEY_TAB;
-    var KEY_REVERSETAB;
+    var KEY_DELETE = 8;
+    var KEY_TAB = 9;
+    var KEY_SHIFT = 16;
+
+    var KEY_LEFTARROW = 37;
+    var KEY_UPARROW = 38;
+    var KEY_RIGHTARROW = 39;
+    var KEY_DOWNARROW = 40;
+
+    var createNode = function() {
+        return $("<div contenteditable></div>").addClass("node");
+    };
 
     var saveData = function() {
         var data = $("#app").html();
@@ -17,108 +26,112 @@ $(document).ready(function() {
         if (getData()) {
             $("#app").html("");
             $("#app").append(getData());
+        } else {
+            $("#app").append(createNode());
         }
     };
 
     initialize();
 
-    $("#app").on("keypress", ".form", function(e) {
-        //when enter-key is pressed, it will create a node.
 
-        var text = $(this).children("input").val();
 
-        //list of if statements into condensed?
+    $("#app").on("keydown", ".node", function(e) {
+
+        console.log(e.keyCode);
+        var text = $(this).text();
+        var html = $(this).html();
+
+        //upon enter key, save the text. and create a sibling node
         if (e.keyCode === KEY_ENTER) {
-            while (text.length <= 0) {
-                text = prompt("insert text");
-            }
 
             e.preventDefault();
-            $node = $("<div></div>").addClass("node").css({
-                "margin": "0 0 0 50px"
-            });
-            var $text = $("<span>" + text + "</span>").addClass("text");
-            $node.prepend($text);
-
-            //jQuery references $(), so you have to create another one...
-            var $childForm = $("<form><input type='text'></form>").addClass("form");
-            var $button = $("<span>*</span>").addClass("button").css({
-                "background-color": "orange"
-            });
-            $node.prepend($button);
-            $node.append($childForm);
-
-            $(this).before($node);
-            saveData();
-
+            $(this).after(createNode());
+            $(this).next().focus();
         }
 
-
-        // if (e.keyCode === KEY_DELETE && text.length === 0) {
-        //     e.preventDefault();
-        //     $(this).children("input").remove();
-        //     saveData();
-        // }
-
-    });
-
-    $("#app").on("click", ".button", function(e) {
-        var $form = $("<form><input  type='text'></form>").addClass("form");
-        var $node = $(this).closest(".node");
-        $node.after($form);
-
-        saveData();
-    });
-
-
-    //when text of the node is clicked,
-    $("#app").on("click", ".text", function(e) {
-        var $form = $("<form><input type='text'></form>").addClass("modifier");
-        $form.children("input").val($(this).text());
-        $(this).before($form);
-        $(this).remove();
-
-        saveData();
-
-    });
-
-    $("#app").on("keypress", ".modifier", function(e) {
-
-        var text = $(this).children("input").val();
-
-        if (e.keyCode === KEY_ENTER) {
+        if (e.keyCode === KEY_DOWNARROW) {
             e.preventDefault();
-            $text = $("<span>" + text + "</span>").addClass("text");
-            $(this).siblings(".button").after($text);
+            $(this).next().focus();
+        }
+
+        if (e.keyCode === KEY_UPARROW) {
+            e.preventDefault();
+            $(this).prev().focus();
+        }
+
+        if (e.keyCode === KEY_DELETE && html.toString().length === 0) {
+        	e.preventDefault();
+        	var content = $(this).html();
+        	$(this).prev().focus(); //the focus functionality should place the text cursor at the end of its content
             $(this).remove();
-            saveData();
         }
+
+        if(e.keyCode === KEY_TAB){
+        	//put the element inside of its previous sibling element.
+        }
+
+        saveData();
+
 
     });
 
-    var timeoutId;
-    $(".button").hover(function() {
-        if (!timeoutId) {
-            var $this = $(this);
-            timeoutId = setTimeout(function() {
-                console.log("hovering");
+    // $("#app").on("click", ".button", function(e) {
+    //     var $form = $("<form><input  type='text'></form>").addClass("form");
+    //     var $node = $(this).closest(".node");
+    //     $node.after($form);
 
-                //turn the background grey (configure in CSS)
-                $this.closest(".node").addClass("highlight");
-            }, 500);
+    //     saveData();
+    // });
 
-        }
 
-    }, function() {
+    // //when text of the node is clicked,
+    // $("#app").on("click", ".text", function(e) {
+    //     var $form = $("<form><input type='text'></form>").addClass("modifier");
+    //     $form.children("input").val($(this).text());
+    //     $(this).before($form);
+    //     $(this).remove();
 
-        if (timeoutId) {
-            console.log("leaving");
-            clearTimeout(timeoutId);
-            timeoutId = false;
-            //turn the background white (configure in CSS)
-            $(this).closest(".node").removeClass("highlight");
-        }
+    //     saveData();
 
-    });
+    // });
+
+    // $("#app").on("keypress", ".modifier", function(e) {
+
+    //     var text = $(this).children("input").val();
+
+    //     if (e.keyCode === KEY_ENTER) {
+    //         e.preventDefault();
+    //         $text = $("<span>" + text + "</span>").addClass("text");
+    //         $(this).siblings(".button").after($text);
+    //         $(this).remove();
+    //         saveData();
+    //     }
+
+    // });
+
+    // var timeoutId;
+    // $(".button").hover(function() {
+    //     if (!timeoutId) {
+    //         var $this = $(this);
+    //         timeoutId = setTimeout(function() {
+    //             console.log("hovering");
+
+    //             //turn the background grey (configure in CSS)
+    //             $this.closest(".node").addClass("highlight");
+    //         }, 500);
+
+    //     }
+
+    // }, function() {
+
+    //     if (timeoutId) {
+    //         console.log("leaving");
+    //         clearTimeout(timeoutId);
+    //         timeoutId = false;
+    //         //turn the background white (configure in CSS)
+    //         $(this).closest(".node").removeClass("highlight");
+    //     }
+
+    // });
 
 });
