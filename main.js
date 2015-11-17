eb7447c1f576f335ac7a7fc4cb5705fe596a8ab0
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-11-17 13:30:52 -0800

    added hovering functionality for bullets

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ $(document).ready(function() {
         let $node = $("<div></div>").addClass("node");
         let $value = $("<div  contenteditable></div>").addClass("value");
         let $children = $("<div></div>").addClass("children");
-        let $point = $("<span>O</span>").addClass("point");
+        let $bullet = $("<span>O</span>").addClass("bullet");
 
         $node.append($value);
         $node.append($children);
-        $node.prepend($point);
+        $node.prepend($bullet);
 
         let $copy = $node.clone();
         return $copy;
@@ -53,226 +53,211 @@ $(document).ready(function() {
         }
     };
 
+    let keydownEvents = function() {
+        $("#app").on("keydown", ".value", function(e) {
 
+            // console.log("You pressed the key with the following keycode",e.keyCode);
+            let textVal = $(this).text();
+            let htmlVal = $(this).html();
+            let $thisNode = $(this).closest(".node");
 
-    initialize();
+            //finds the nearest previous sibling node or closest parent node
+            let findOneUp = function() {
+                let $adjacent = $thisNode.prev();
 
+                //if thisNode does not have an older sibling, return the current node's parent
+                if ($adjacent.length === 0) {
+                    //closest 
 
-
-    $("#app").on("keydown", ".value", function(e) {
-
-        // console.log("You pressed the key with the following keycode",e.keyCode);
-        let textVal = $(this).text();
-        let htmlVal = $(this).html();
-        let $thisNode = $(this).closest(".node");
-
-        //finds the nearest previous sibling node or closest parent node
-        let findOneUp = function() {
-            let $adjacent = $thisNode.prev();
-
-            //if thisNode does not have an older sibling, return the current node's parent
-            if ($adjacent.length === 0) {
-                //closest 
-
-                return $thisNode.parent().closest(".node");
-            } else {
-                //has an older sibling
-
-                if ($adjacent.children(".children").children(".node").length === 0) {
-                    //the older sibling has no children.
-                    return $adjacent;
+                    return $thisNode.parent().closest(".node");
                 } else {
-                    //the older sibling has children.
-                    let length = $adjacent.children(".children").children(".node").length;
-                    let adjacent = $adjacent.children(".children").children(".node")[length - 1];
+                    //has an older sibling
+
+                    if ($adjacent.children(".children").children(".node").length === 0) {
+                        //the older sibling has no children.
+                        return $adjacent;
+                    } else {
+                        //the older sibling has children.
+                        let length = $adjacent.children(".children").children(".node").length;
+                        let adjacent = $adjacent.children(".children").children(".node")[length - 1];
 
 
 
-                    while ($(adjacent).children(".children").children(".node").length > 0) {
-                        length = $(adjacent).children(".children").children(".node").length;
-                        adjacent = $(adjacent).children(".children").children(".node")[length - 1];
+                        while ($(adjacent).children(".children").children(".node").length > 0) {
+                            length = $(adjacent).children(".children").children(".node").length;
+                            adjacent = $(adjacent).children(".children").children(".node")[length - 1];
+                        }
+
+                        return adjacent;
                     }
-
-                    return adjacent;
                 }
-            }
-        };
+            };
 
-        console.log("node's adjacentUp value: ", $(findOneUp()).children(".value").text());
-        console.log("node's value: ", $(this).text());
-        // console.log("node's adjacentDown value: ",$(findOneUp()).children(".value").text());
-
+            console.log("node's adjacentUp value: ", $(findOneUp()).children(".value").text());
+            console.log("node's value: ", $(this).text());
+            // console.log("node's adjacentDown value: ",$(findOneUp()).children(".value").text());
 
 
-        let findOneDown = function() {
-            let $adjacent = $thisNode.find(".node");
-            //console.log($adjacent);
-            if ($adjacent.length !== 0) {
-                //if children exist
-                console.log("child");
-                return $adjacent[0];
-            } else {
-                //if child doesn't exist, look into its sibling.
-                let $sibling = $thisNode.next();
-                if ($sibling.length > 0) {
-                    //if sibling exists, return the sibling.
-                    console.log("sibling");
-                    return $sibling;
+
+            let findOneDown = function() {
+                let $adjacent = $thisNode.find(".node");
+                //console.log($adjacent);
+                if ($adjacent.length !== 0) {
+                    //if children exist
+                    console.log("child");
+                    return $adjacent[0];
                 } else {
-                    //if the sibling does not exist, then move to its parent's sibling
-                    console.log("parent's sibling");
-                    let $parent = $thisNode.parent().closest(".node");
-                    let $parentSibling = $parent.next();
-                    while ($parentSibling.length === 0) {
-                    	//if sibling doesn't exist
-                    	if($parent.length === 0){
-                    		return $thisNode;
-                    	}
-                        $parent = $parent.parent().closest(".node");
-                        $parentSibling = $parent.next();
+                    //if child doesn't exist, look into its sibling.
+                    let $sibling = $thisNode.next();
+                    if ($sibling.length > 0) {
+                        //if sibling exists, return the sibling.
+                        console.log("sibling");
+                        return $sibling;
+                    } else {
+                        //if the sibling does not exist, then move to its parent's sibling
+                        console.log("parent's sibling");
+                        let $parent = $thisNode.parent().closest(".node");
+                        let $parentSibling = $parent.next();
+                        while ($parentSibling.length === 0) {
+                            //if sibling doesn't exist
+                            if ($parent.length === 0) {
+                                return $thisNode;
+                            }
+                            $parent = $parent.parent().closest(".node");
+                            $parentSibling = $parent.next();
+                        }
+                        return $parentSibling;
                     }
-                    return $parentSibling;
                 }
-            }
 
-            //if there is no sibling, move to thisNode's sibling and 
-        };
-
+                //if there is no sibling, move to thisNode's sibling and 
+            };
 
 
-        let moveOneUp = function() {
-            let $oneup = $(findOneUp());
-            $oneup.children(".value").focus();
-        };
 
-        let moveOneDown = function() {
-            $(findOneDown()).children(".value").focus();
-        };
+            let moveOneUp = function() {
+                let $oneup = $(findOneUp());
+                $oneup.children(".value").focus();
+            };
 
-        if (e.keyCode === KEY_SHIFT) {
-            e.preventDefault();
-            moveOneDown();
-        }
+            let moveOneDown = function() {
+                $(findOneDown()).children(".value").focus();
+            };
 
-        //ENTER: Create a new sibling node. Focus on the newly created sibling node.
-        if (e.keyCode === KEY_ENTER) {
-            e.preventDefault();
-            $thisNode.after(createNode());
-            $thisNode.next().children(".value").focus();
-        }
-
-        //DOWNARROW: focus on the next node
-        if (e.keyCode === KEY_DOWNARROW) {
-            e.preventDefault();
-            // $thisNode.next().children(".value").focus();
-            moveOneDown();
-        }
-
-        //UPARROW: Focus on the previous node
-        if (e.keyCode === KEY_UPARROW) {
-            e.preventDefault();
-            // $(findOneUp()).children(".value").focus();
-            moveOneUp();
-        }
-
-        //LEFT ARROW + caret at beginning: move to the previous node
-        if (e.keyCode === KEY_LEFTARROW) {
-            if ($(this).caret() === 0) {
-                e.preventDefault();
-                moveOneUp();
-            }
-        }
-
-        //RIGHT ARROW + caret at end: move to the next node.
-        if (e.keyCode === KEY_RIGHTARROW) {
-
-            let length = $(this).text().length;
-            if ($(this).caret() === length) {
+            if (e.keyCode === KEY_SHIFT) {
                 e.preventDefault();
                 moveOneDown();
-                // let $nextNodeValue = $thisNode.next().children(".value");
-                // $nextNodeValue.focus();
             }
-        }
 
-        //DELETE: Remove the node. Focus on the previous node.
-        if (e.keyCode === KEY_DELETE) {
-
-            if (htmlVal.toString().length === 0) {
+            //ENTER: Create a new sibling node. Focus on the newly created sibling node.
+            if (e.keyCode === KEY_ENTER) {
                 e.preventDefault();
+                $thisNode.after(createNode());
+                $thisNode.next().children(".value").focus();
+            }
 
-                if ($("#app").children().length > 1 || $thisNode.parent().closest(".node").length > 0) {
+            //DOWNARROW: focus on the next node
+            if (e.keyCode === KEY_DOWNARROW) {
+                e.preventDefault();
+                // $thisNode.next().children(".value").focus();
+                moveOneDown();
+            }
+
+            //UPARROW: Focus on the previous node
+            if (e.keyCode === KEY_UPARROW) {
+                e.preventDefault();
+                // $(findOneUp()).children(".value").focus();
+                moveOneUp();
+            }
+
+            //LEFT ARROW + caret at beginning: move to the previous node
+            if (e.keyCode === KEY_LEFTARROW) {
+                if ($(this).caret() === 0) {
+                    e.preventDefault();
                     moveOneUp();
-                    //http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area
-                    $thisNode.remove();
-                    //if the current node is not empty & the previous node is empty + caret at the beginning, delete the previous node.
+                }
+            }
+
+            //RIGHT ARROW + caret at end: move to the next node.
+            if (e.keyCode === KEY_RIGHTARROW) {
+
+                let length = $(this).text().length;
+                if ($(this).caret() === length) {
+                    e.preventDefault();
+                    moveOneDown();
+                    // let $nextNodeValue = $thisNode.next().children(".value");
+                    // $nextNodeValue.focus();
+                }
+            }
+
+            //DELETE: Remove the node. Focus on the previous node.
+            if (e.keyCode === KEY_DELETE) {
+
+                if (htmlVal.toString().length === 0) {
+                    e.preventDefault();
+
+                    if ($("#app").children().length > 1 || $thisNode.parent().closest(".node").length > 0) {
+                        moveOneUp();
+                        //http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area
+                        $thisNode.remove();
+                        //if the current node is not empty & the previous node is empty + caret at the beginning, delete the previous node.
+                    }
+
                 }
 
             }
 
-        }
+            //TAB: Move the node inside of its previous sibling node. Label the moved node as a child node.
+            if (e.keyCode === KEY_TAB) {
+                e.preventDefault();
+                let $prevNodeChildren = $thisNode.prev().children(".children");
+                $prevNodeChildren.append($thisNode);
+                $prevNodeChildren.children(".node").children(".value").focus();
+            }
 
-        //TAB: Move the node inside of its previous sibling node. Label the moved node as a child node.
-        if (e.keyCode === KEY_TAB) {
-            e.preventDefault();
-            let $prevNodeChildren = $thisNode.prev().children(".children");
-            $prevNodeChildren.append($thisNode);
-            $prevNodeChildren.children(".node").children(".value").focus();
+            //REVERSE TAB: Move the child node outside of its parent node(i.e. next)
+            //http://stackoverflow.com/questions/10655202/detect-multiple-keys-on-single-keypress-event-on-jquery 
 
-        }
+            //ENTER + Nothing in the node: Same functionality as REVERSE TAB (see above)
+            if (e.keyCode === KEY_ENTER && htmlVal.toString().length === 0) {
 
-        //REVERSE TAB: Move the child node outside of its parent node(i.e. next)
-        //http://stackoverflow.com/questions/10655202/detect-multiple-keys-on-single-keypress-event-on-jquery 
+            }
 
-        //ENTER + Nothing in the node: Same functionality as REVERSE TAB (see above)
-        if (e.keyCode === KEY_ENTER && htmlVal.toString().length === 0) {
-
-        }
-
-        saveData();
-
-
-    });
-
-    // //when text of the node is clicked,
-    // $("#app").on("click", ".text", function(e) {
-    //     let $form = $("<form><input type='text'></form>").addClass("modifier");
-    //     $form.children("input").val($(this).text());
-    //     $(this).before($form);
-    //     $(this).remove();
-
-    //     saveData();
-
-    // });
-
-
-    let hoverButton = function() {
-        // let timeoutId;
-        // $(".button").hover(function() {
-        //     if (!timeoutId) {
-        //         let $this = $(this);
-        //         timeoutId = setTimeout(function() {
-        //             console.log("hovering");
-
-        //             //turn the background grey (configure in CSS)
-        //             $this.closest(".node").addClass("highlight");
-        //         }, 500);
-
-        //     }
-
-        // }, function() {
-
-        //     if (timeoutId) {
-        //         console.log("leaving");
-        //         clearTimeout(timeoutId);
-        //         timeoutId = false;
-        //         //turn the background white (configure in CSS)
-        //         $(this).closest(".node").removeClass("highlight");
-        //     }
-
-        // });
-
+            saveData();
+        });
     };
+
+    let hoverEvents = function() {
+
+
+        let hoverBullet = function() {
+            let timeoutId = false;
+
+            $("#app").on("mouseenter", ".bullet", function() {
+                if (!timeoutId) {
+                    let $this = $(this);
+                    timeoutId = setTimeout(function() {
+                        console.log("hovering");
+                    }, 500);
+                }
+            });
+
+            $("#app").on("mouseleave", ".bullet", function() {
+                if (timeoutId) {
+                    console.log("leaving");
+                    clearTimeout(timeoutId);
+                    timeoutId = false;
+                }
+            });
+        };
+
+        hoverBullet();
+    };
+
+    initialize();
+    keydownEvents();
+    hoverEvents();
+
 
 
 });
