72ac21750174a216da5fcb6294d79f5487009f13
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-11-21 16:16:17 -0800

    minor fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("../static"));
+app.use(express.static("../client"));
 
 app.listen(4000, function(){
 	console.log("server running at 4000");
