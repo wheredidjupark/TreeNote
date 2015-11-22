5e2e73c3fcf1e23b5f72870e66942258e5dd6159
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-11-22 00:24:46 -0800

    moved files

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,28 +6,29 @@ module.exports = function(grunt) {
     grunt.initConfig({
         uglify: {
             main: {
-                src: ["scripts/main.js"],
-                dest: "scripts/main.min.js"
+                src: ["client/scripts/main.js"],
+                dest: "client/scripts/main.min.js"
             }
         },
         sass: {
             dist: {
-                src: ["styles/main.scss"],
-                dest: "styles/main.css"
+                src: ["client/styles/main.scss"],
+                dest: "client/styles/main.css"
             }
         },
         cssmin: {
             main: {
-                src: "styles/main.css",
-                dest: "styles/main.min.css"
+                src: "client/styles/main.css",
+                dest: "client/styles/main.min.css"
             }
-        },
-        jshint: {
-            src: ["Gruntfile.js", "scripts/main.js"]
         }
 
     });
 
+
+        // jshint: {
+        //     src: ["Gruntfile.js", "client/scripts/main.js"]
+        // }
     grunt.registerTask('default', ['uglify',  'sass', 'cssmin']);
     grunt.registerTask("build", ['uglify', 'sass', 'cssmin']);
 
