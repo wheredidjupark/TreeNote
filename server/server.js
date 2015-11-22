088f6d89efd79ff91c3b64fa03b2cb9f314430e7
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-11-22 00:39:00 -0800

    serving static files finally workinggggggggggg

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-app.use("/", express.static("../client"));
+app.use("/", express.static(__dirname + "/../client"));
 
 app.use(bodyParser.urlencoded({
     extended: true
